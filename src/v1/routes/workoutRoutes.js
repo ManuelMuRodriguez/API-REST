e96252e8105333e69b06e0d6bd2a204592b61a95
Manuel Muñoz Rodríguez <mmr411@ual.es>
2023-09-28 13:18:42 +0200

    Desarrollo de los controladores

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,27 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+// Importamos el controlador
+
+const workoutController = require("../../controllers/workoutController")
+
 
 router
-    .get('/', (req, res) => {
-    res.send('Get all workouts');
-    })
-
-    .get("/:workoutId", (req, res) => {
-        res.send(`Get workout ${req.params.workoutId}`);
-    })
-
-    .post('/:workoutId', (req, res) => {
-        res.send(`Create workout ${req.params.workoutId}`)
-    })
-
-    .patch('/:workoutId', (req, res) => {
-        res.send(`Update workout ${req.params.workoutId}`)
-    })
-
-    .delete('/:workoutId', (req, res) => {
-        res.send(`Delete workout ${req.params.workoutId}`)
-    });
+    .get('/', workoutController.getAllWorkouts)
+    .get("/:workoutId", workoutController.getOneWorkout)
+    .post('/:workoutId', workoutController.createNewWorkout)
+    .patch('/:workoutId', workoutController.updateOneWorkout)
+    .delete('/:workoutId', workoutController.deleteOneWorkout);
 
 module.exports = router;
 
